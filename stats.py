561cbbce80bf0eff4e6e3557e7321f38b9fdd335
EyeZiS <tabhooked@gmail.com>
2017-12-06 02:33:47 +0200

    tweak stats period to 5 days

--- stats.py
+++ stats.py
@@ -17,15 +17,16 @@ DESTINATION_CHAT = os.environ['spoilero_stats_destination']
 
 db_cursor = Database().get_cursor()
 CURRENT_TIMESTAMP = timestamp_floor(24*3600)
-CUTOFF_TIMESTAMP = CURRENT_TIMESTAMP - 24*3600
+CUTOFF_TIMESTAMP = CURRENT_TIMESTAMP - 24*3600*5
+YESTERDAY_TIMESTAMP = CURRENT_TIMESTAMP - 24*3600
 CUTOFF_TIME = datetime.utcfromtimestamp(CUTOFF_TIMESTAMP)
 CURRENT_TIME = datetime.utcfromtimestamp(CURRENT_TIMESTAMP)
-bins = int(CURRENT_TIMESTAMP - CUTOFF_TIMESTAMP) // REQUEST_COUNT_RESOLUTION
+bins = 720
 
 # setup plot
 fig, ax1 = plt.subplots(figsize=(9,5))
 plt.title('{} (until {})'.format(
-    'Statistics for the past 24 hours',
+    'Statistics for the past 5 days',
     CURRENT_TIME.strftime('%Y-%m-%d %H:%M UTC')
 ))
 ax1.set_xlabel('Time (UTC)')
@@ -42,7 +43,9 @@ requests = db_cursor.fetchall()
 # process data
 x = [datetime.utcfromtimestamp(request['timestamp']) for request in requests]
 counts = [request['count'] for request in requests]
-requests_today = sum(counts)
+requests_today = sum(
+    request['count'] for request in requests if request['timestamp'] >= YESTERDAY_TIMESTAMP
+)
 
 # plot data
 ax1.hist(x, bins=bins, weights=counts, color='xkcd:sky blue')
@@ -64,13 +67,14 @@ x = [datetime.utcfromtimestamp(timestamp) for timestamp in spoilers]
 y = list(range(total_spoilers))
 x.append(CURRENT_TIME)
 y.append(y[-1])
-spoilers_today = sum(1 for timestamp in spoilers if timestamp >= CUTOFF_TIMESTAMP)
+spoiler_count = sum(1 for timestamp in spoilers if timestamp >= CUTOFF_TIMESTAMP)
+spoilers_today = sum(1 for timestamp in spoilers if timestamp >= YESTERDAY_TIMESTAMP)
 
 # plot data
 ax2 = ax1.twinx()
-ax2.xaxis.set_major_formatter(md.DateFormatter('%H:%M'))
+ax2.xaxis.set_major_formatter(md.DateFormatter('%m/%d'))
 ax2.plot(x, y, color='xkcd:red')
-ax2.axis(xmin=CUTOFF_TIME, xmax=CURRENT_TIME, ymin=total_spoilers - spoilers_today - 1)
+ax2.axis(xmin=CUTOFF_TIME, xmax=CURRENT_TIME, ymin=total_spoilers - spoiler_count - 1)
 ax2.set_ylabel('Total spoilers', color='xkcd:red')
 ax2.tick_params('y', colors='xkcd:red')
 
